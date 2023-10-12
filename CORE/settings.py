7bf18378bc890571b6a774179133c4b04ba63915
BillRustamov <bill.rustamov@gmail.com>
2023-10-11 22:37:52 -0400

    Update ALLOWED_HOSTS

--- CORE/settings.py
+++ CORE/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "dev-search-test-project.up.railway.app"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "dev-search-my-project.up.railway.app"]
 
 
 # Application definition
